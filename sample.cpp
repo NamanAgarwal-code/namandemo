c54670153bafac3896dd1c6db0141c9fb126e49b
Naman Agarwal <agarwalnaman1306@gmail.com>
2025-03-20 11:01:57 +0530

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+class Hero {
+    //properties
+  public:
+  int health;
+  char level;  
+void print(){
+  cout<<level<<endl;
+}
+
+
+};
+
+int main() {
+    //creation of object
+    Hero ramesh;
+    ramesh.health=70;
+    ramesh.level='A';
+
+
+    cout << "health is;"<<ramesh.health<<endl;
+    cout << "level is;"<<ramesh.level<<endl;
+    
+    //cout << "size: " << sizeof(h1) << endl;
+    return 0;
+}
